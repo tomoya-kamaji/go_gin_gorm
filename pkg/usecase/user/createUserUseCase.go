9f2da6d489c2a6031a1af9935b5326ba849fda6a
tomo_kamaji <tomoya.kamaji@gmail.com>
2022-10-23 17:40:38 +0900

    CreateUserUsecaseImplを作成

--- pkg/usecase/user/createUserUseCase.go
+++ pkg/usecase/user/createUserUseCase.go
@@ -5,23 +5,23 @@ import (
 	"yu-croco/ddd_on_golang/pkg/errors"
 )
 
-type createUserUsecaseImpl struct {
+type CreateUserUsecaseImpl struct {
 	UserName          string
 	UserRepository  user.UserRepository
 }
 
-type createUserUsecase interface {
+type CreateUserUsecase interface {
 	Run() (*user.User, *errors.AppError)
 }
 
-func NewCreateUserUsecaseImpl(userName string, userRepository user.UserRepository) createUserUsecase {
-	return createUserUsecaseImpl{
+func NewCreateUserUsecaseImpl(userName string, userRepository user.UserRepository) CreateUserUsecase {
+	return CreateUserUsecaseImpl{
 		UserName:          userName,
 		UserRepository:  userRepository,
 	}
 }
 
-func (impl createUserUsecaseImpl) Run() (*user.User, *errors.AppError) {
+func (impl CreateUserUsecaseImpl) Run() (*user.User, *errors.AppError) {
 	userId := user.CreateUserId()
 	userName,userNameErr := user.NewUserName(impl.UserName)
 	if userNameErr.HasErrors() {
