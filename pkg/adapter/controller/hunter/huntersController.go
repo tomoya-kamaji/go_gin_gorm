9f8223b3074d47b62991f109c0605c4eff73b270
tomo_kamaji <tomoya.kamaji@gmail.com>
2022-11-18 09:17:49 +0900

    elasticsearch

--- pkg/adapter/controller/hunter/huntersController.go
+++ pkg/adapter/controller/hunter/huntersController.go
@@ -1,11 +1,13 @@
 package hunter
 
 import (
-	"github.com/gin-gonic/gin"
 	helpers2 "yu-croco/ddd_on_golang/pkg/adapter/controller/helpers"
 	model2 "yu-croco/ddd_on_golang/pkg/domain/model"
+	elasticsearch "yu-croco/ddd_on_golang/pkg/infrastructure/elasticSearch"
 	"yu-croco/ddd_on_golang/pkg/infrastructure/queryImpl/hunter"
 	repositoryImpl2 "yu-croco/ddd_on_golang/pkg/infrastructure/repositoryImpl"
+
+	"github.com/gin-gonic/gin"
 )
 
 type HuntersController struct{}
@@ -23,6 +25,9 @@ func (ctrl HuntersController) Show(c *gin.Context) {
 }
 
 func (ctrl HuntersController) Index(c *gin.Context) {
+	esClient := elasticsearch.ElasticsearchClient{}
+	esClient.CreateIndex()
+
 	result := hunter.NewHunterQueryImpl().FindAll()
 	helpers2.Response(c, result, nil)
 }
--- pkg/infrastructure/elasticSearch/elasticSearchClient.go
+++ pkg/infrastructure/elasticSearch/elasticSearchClient.go
@@ -1,23 +1,52 @@
 package elasticsearch
 
 import (
+	"fmt"
 	"log"
+	"strings"
 
 	"github.com/elastic/go-elasticsearch/v7"
 )
 
 type ElasticsearchClient struct{}
 
-func (ctrl ElasticsearchClient) Update() {
-	es, err := elasticsearch.NewDefaultClient()
+
+func (ctrl ElasticsearchClient) CreateIndex() {
+	// ここの設定の見直しを行う
+	cfg := elasticsearch.Config{
+		Addresses: []string{
+			"https://localhost:9200",
+		},
+	}
+	es, err := elasticsearch.NewClient(cfg)
 	if err != nil {
 		log.Fatalf("Error creating the client: %s", err)
 	}
 
-	res, err := es.Info()
 	if err != nil {
 		log.Fatalf("Error getting response: %s", err)
 	}
-	defer res.Body.Close()
+	index := "test"
+	mapping := `
+	{
+		"settings": {
+			"number_of_shards": 1
+		},
+		"mappings": {
+			"properties": {
+				"name": {
+					"type": "text"
+				}
+			}
+		}
+	}`
+	res, err := es.Indices.Create(
+			index,
+			es.Indices.Create.WithBody(strings.NewReader(mapping)),
+	)
+	if err != nil {
+			log.Fatal(err)
+	}
+	fmt.Printf("\"更新前です\": %v\n", "ああ")
 	log.Println(res)
 }
