6debcfec8af1fc7bca4ef1848b4e07da39873965
tomo_kamaji <tomoya.kamaji@gmail.com>
2022-12-05 09:16:46 +0900

    一旦コミット

---
+++ pkg/adapter/controller/cli/dbParallel.go
@@ -0,0 +1,47 @@
+package controller
+
+import (
+	"fmt"
+	"sync"
+	"time"
+	"yu-croco/ddd_on_golang/pkg/domain/user"
+	"yu-croco/ddd_on_golang/pkg/infrastructure"
+	"yu-croco/ddd_on_golang/pkg/infrastructure/dto"
+
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+)
+
+type ClisController struct{}
+
+func (ctrl ClisController) Run(c *gin.Context) {
+	db := infrastructure.GetDB()
+	now := time.Now()
+	var wg sync.WaitGroup
+	wg.Add(1)
+	for i := 0; i < 5; i++ {
+		go createUser(db)
+	}
+	fmt.Printf("経過: %vms\n", time.Since(now).Milliseconds())
+}
+
+func createUser(
+	db *gorm.DB,
+){
+	userEntities := []dto.UserEntity{}
+	for i := 0; i < 1000; i++ {
+		userEntity := dto.UserEntity{}
+		userID := user.CreateUserId()
+		user := user.NewUser(*userID, user.UserName("tomoya"))
+		if db.Find(&userEntity, dto.UserEntity{ID: uint(user.Id)}).RecordNotFound() {
+			userEntity.ID = uint(user.Id)
+			userEntity.Name = string(user.Name)
+		} else {
+			userEntity.Name = string(user.Name)
+		}
+		userEntities = append(userEntities, userEntity)
+	}
+	for _, userEntity := range userEntities {
+		db.Save(&userEntity)
+	}
+}
---
+++ pkg/adapter/controller/cli/db_test.go
@@ -0,0 +1,104 @@
+package controller
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestDo(t *testing.T) {
+	t.Run("並行処理テスト", func(t *testing.T) {
+		main()
+	})
+}
+
+func fibonacci(c, quit chan int) {
+	x, y := 0, 1
+	for {
+		select {
+		case c <- x:
+			x, y = y, x+y
+		case <-quit:
+			fmt.Println("quit")
+			return
+		}
+	}
+}
+
+func omikuji(c1, c2, c3 chan int) {
+	for i := 0; i < 10; i++ {
+		select {
+		case <-c1:
+			fmt.Println("大吉")
+		case <-c2:
+			fmt.Println("中吉")
+		case <-c3:
+			fmt.Println("小吉")
+		}
+	}
+}
+
+// func main() {
+// 	c1 := make(chan int)
+// 	c2 := make(chan int)
+// 	c3 := make(chan int)
+// 	// omikuji(c1,c2,c3)
+// 	result := string[3]
+// 	for i := 0; i < 10; i++ {
+// 		select {
+// 		case <-c1:
+// 			fmt.Println("大吉")
+// 		case <-c2:
+// 			fmt.Println("中吉")
+// 		case <-c3:
+// 			fmt.Println("小吉")
+// 		}
+// 	}
+// 	fmt.Println("終了")
+// }
+
+func main() {
+	var wg sync.WaitGroup
+	ch1 := make(chan rune)
+	ch2 := make(chan int)
+	done := make(chan struct{})
+
+	go func() {
+		for {
+			select {
+			case c := <-ch1:
+				fmt.Printf("[R1] %c\n", c)
+			case i := <-ch2:
+				fmt.Println("[R2]", i)
+			case <-done:
+				fmt.Println("Done!")
+				return
+			}
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for c := 'A'; c <= 'C'; c++ {
+			ch1 <- c
+			time.Sleep(time.Second)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for i := 1; i < 5; i++ {
+			ch2 <- i
+			time.Sleep(500 * time.Millisecond)
+		}
+	}()
+
+	wg.Wait()
+
+	close(done)
+
+	time.Sleep(time.Second)
+}
