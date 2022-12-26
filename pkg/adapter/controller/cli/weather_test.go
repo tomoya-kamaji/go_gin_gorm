8b7125176bd3ad47af3243111c265f665c592731
tomoya <tomoya@tomoyanoMacBook-Pro.local>
2022-12-26 14:50:42 +0900

    並列処理の実装

--- pkg/adapter/controller/cli/weather_test.go
+++ pkg/adapter/controller/cli/weather_test.go
@@ -15,19 +15,35 @@ import (
 func TestWeather(t *testing.T) {
 	t.Run("並行処理テスト", func(t *testing.T) {
 		now := time.Now()
-		main3()
+		parallel()
+		// single()
 		fmt.Printf("経過: %vms\n", time.Since(now).Seconds())
 	})
 }
 
-func main3() {
+func parallel() {
 	weatherFetcher := WeatherFetcher{}
-	cityCode := "400040"
-	weather := weatherFetcher.fetch(cityCode)
-
 	records := [][]string{}
-	// ここをループで回す。来たものから作成する
-	records = append(records, []string{weather.Title, weather.Forecasts[0].Date, weather.Forecasts[0].Telop, weather.Link})
+	// 送信
+	weatherChan := make(chan Weather, len(cityCodes))
+	for _, cityCode := range cityCodes {
+		go weatherFetcher.parallelFetch(weatherChan, cityCode)
+	}
+
+	// 受信
+	for weather := range weatherChan {
+		records = append(records, []string{weather.Title, weather.Forecasts[0].Date, weather.Forecasts[0].Telop, weather.Link})
+	}
+	uploadCsv(records)
+}
+
+func single() {
+	weatherFetcher := WeatherFetcher{}
+	records := [][]string{}
+	for _, cityCode := range cityCodes {
+		weather := weatherFetcher.fetch(cityCode)
+		records = append(records, []string{weather.Title, weather.Forecasts[0].Date, weather.Forecasts[0].Telop, weather.Link})
+	}
 	uploadCsv(records)
 }
 
@@ -37,7 +53,16 @@ func (f WeatherFetcher) fetch(cityCode string) Weather {
 	// 天気予報APIのつなぎ込み
 	url := "https://weather.tsukumijima.net/api/forecast/city/" + cityCode
 	weather := getWeather(url)
+
 	return weather
+}
+
+func (f WeatherFetcher) parallelFetch(weatherChan chan Weather, cityCode string) {
+	time.Sleep(time.Second * 1)
+	url := "https://weather.tsukumijima.net/api/forecast/city/" + cityCode
+	weather := getWeather(url)
+	weatherChan <- weather
+	close(weatherChan)
 }
 
 type Weather struct {
@@ -69,6 +94,7 @@ func uploadCsv(records [][]string) {
 }
 
 func getWeather(url string) Weather {
+	time.Sleep(time.Second * 1)
 	response, err := http.Get(url)
 	if err != nil {
 		log.Fatalf("Connection Error: %v", err)
@@ -83,4 +109,19 @@ func getWeather(url string) Weather {
 		log.Fatal(err)
 	}
 	return weather
+}
+
+var cityCodes = []string{
+	"130010",
+	"140010",
+	"150010",
+	"160010",
+	"170010",
+	"180010",
+	"190010",
+	"200010",
+	"210010",
+	"220010",
+	"230010",
+	"240010",
 }
