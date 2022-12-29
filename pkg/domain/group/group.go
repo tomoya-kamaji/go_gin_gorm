38e7f33fa11f0473be35d5c77be6e3c4cddbe89e
tomoya <tomoya@tomoyanoMacBook-Pro.local>
2022-12-29 16:29:44 +0900

    groupドメイン作成

--- pkg/domain/group/group.go
+++ pkg/domain/group/group.go
@@ -1,6 +1,9 @@
 package group
 
-import "yu-croco/ddd_on_golang/pkg/domain/user"
+import (
+	"yu-croco/ddd_on_golang/pkg/domain/user"
+	"yu-croco/ddd_on_golang/pkg/errors"
+)
 
 type Group struct {
 	Id      GroupId       `json:"groupId"`
@@ -12,6 +15,23 @@ func NewGroup(id GroupId, name string) *Group {
 	return &Group{Id: id, Name: name, UserIds: make([]user.UserId, 0)}
 }
 
-func (group *Group) AddUser(userId user.UserId) {
+func (group *Group) AddUser(userId user.UserId) *errors.AppError {
+	//userIdsにuserIdが存在するか確認
+	for _, id := range group.UserIds {
+		if id == userId {
+			err := errors.NewAppError("同一ユーザは追加できない")
+			return &err
+		}
+	}
 	group.UserIds = append(group.UserIds, userId)
+	return nil
+}
+
+func (group *Group) RemoveUser(userId user.UserId) {
+	for i, id := range group.UserIds {
+		if id == userId {
+			group.UserIds = append(group.UserIds[:i], group.UserIds[i+1:]...)
+			break
+		}
+	}
 }
--- pkg/domain/group/group_test.go
+++ pkg/domain/group/group_test.go
@@ -2,30 +2,54 @@ package group
 
 import (
 	"testing"
+	"yu-croco/ddd_on_golang/pkg/domain/user"
+	"yu-croco/ddd_on_golang/pkg/errors"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestNewGroup(t *testing.T) {
-	// cases := map[string]struct {
-	// 	Id      UserId
-	// 	Name    UserName
-	// 	want    *User
-	// 	wantErr error
-	// }{
-	// 	"NewUser": {
-	// 		Id:   UserId(1),
-	// 		Name: UserName("tomoya"),
-	// 		want: &User{Id: UserId(1), Name: UserName("tomoya")},
-	// 	},
-	// }
+	cases := map[string]struct {
+		Id      GroupId
+		Name    string
+		want    *Group
+		wantErr error
+	}{
+		"NewGroup": {
+			Id:   GroupId(1),
+			Name: "エンジニアチーム",
+			want: &Group{Id: GroupId(1), Name: "エンジニアチーム", UserIds: make([]user.UserId, 0)},
+		}}
 
-	// for name, tc := range cases {
-	// 	t.Run(name, func(t *testing.T) {
-	// 		res := NewUser(tc.Id, tc.Name)
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			res := NewGroup(tc.Id, tc.Name)
+			if diff := cmp.Diff(res, tc.want); diff != "" {
+				t.Errorf("X value is mismatch (-want +got):%s\n", diff)
+			}
+		})
+	}
+}
 
-	// 		if diff := cmp.Diff(res, tc.want); diff != "" {
-	// 			t.Errorf("X value is mismatch (-want +got):%s\n", diff)
-	// 		}
-
-	// 	})
-	// }
+func TestAddUser(t *testing.T) {
+	cases := map[string]struct {
+		group   *Group
+		userId  user.UserId
+		want    []user.UserId
+		wantErr *errors.AppError
+	}{
+		"正常パターン：指定したユーザが追加される": {
+			group:  NewGroup(GroupId(1), "エンジニアチーム"),
+			userId: user.UserId(1),
+			want:   make([]user.UserId, 0),
+		},
+	}
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			res := tc.group.UserIds
+			if diff := cmp.Diff(res, tc.want); diff != "" {
+				t.Errorf("value is mismatch (-want +got):%s\n", diff)
+			}
+		})
+	}
 }
